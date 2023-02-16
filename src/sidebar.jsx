b6e065ab1e21000174f4613557e8ca2aea1161a4
Sumer Vardhan <sv110@duke.edu>
2023-02-16 01:34:07 -0600

    added css transpile to webpack

--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import './styles/sidebar.css';
 
 function EmailInput () {
     return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: {
@@ -11,25 +12,35 @@ module.exports = {
         filename: '[name].js'
     },
     module: {
-        rules : [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
+        rules : 
+        [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
                 }
-            }
-        }]
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
         template: '/src/sidebar.html',
-        filename: 'sidebar.html'
-    }),
+        filename: '[name].html'
+        }),
     new CopyPlugin({
         patterns: [
           { from: "public" },
-        ]})
+        ]}),
+    new MiniCssExtractPlugin({
+        filename: 'styles/[name].css',
+        }),
     ],
 };
